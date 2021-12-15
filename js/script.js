fbff35496ab883bdb80dfb47c0872757f0b2c2e7
Blazej Duszynski <bduszynski92@gmail.com>
2021-12-15 23:17:39 +0100

    adding tasks repaired

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@
     }
 
     const toggleTaskDone = (index) => {
-        tasks = tasks.map((task, taskIndex) => (taskIndex === index) ? ({ ...task, done: !task.done }) : ({ ...task }));
+        tasks = tasks.map ((task, taskIndex) => (taskIndex === index) ? ({...task, done: !task.done}) : ({...task}));
 
         render();
 
@@ -30,14 +30,6 @@
         }));
 
         render();
-    };
-
-    const bindButtonEvents = () => {
-        const markAllTasksDoneButton = document.querySelector(".section__buttons--markAllTasksDone");
-
-        markAllTasksDoneButton.addEventListener("click", () => {
-            markAllTasksDone();
-        });
     };
 
     const toggleHideDoneTasks = () => {
@@ -100,7 +92,6 @@
 
     const init = () => {
         render();
-        bindButtonEvents();
 
         const form = document.querySelector(".form");
 
